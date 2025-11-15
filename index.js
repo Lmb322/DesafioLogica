8743b9b80f25f14aa86683f542e3d71e42d2f5d2
Lmb22 <lucas.belgoni@gmail.com>
2025-11-15 20:29:44 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Olá mundão")
+let nomeHeroi = "Robervaldo"
+let xpHeroi = 2000
+
+if (xpHeroi <=1000){
+    nivel = "Ferro"
+}else if (xpHeroi <=2000){
+    nivel = "Bronze"
+}else if (xpHeroi <=5000){
+    nivel = "Prata"
+}else if (xpHeroi <=7000){
+    nivel = "Ouro"
+}else if (xpHeroi <=8000){
+    nivel = "Platina"
+}else if (xpHeroi <=9000){
+    nivel = "Ascendente"
+}else if (xpHeroi <=10000){
+    nivel = "Imortal"
+}else if (xpHeroi >10001){
+    nivel = "Radiante"
+}
+ 
+ 
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
---
+++ teste.js
@@ -0,0 +1,6 @@
+let nomeHeroi = "Robervaldo"
+let xpHeroi = 2000
+
+if (xpHeroi)
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel)
