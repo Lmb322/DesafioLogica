066c59e9844986b0ec6579bcfd4d3ea193afd5e5
Lucas Belgoni <lucas.belgoni@gmail.com>
2025-11-17 08:39:28 -0300

    Atualizar o desafio2.js

--- desafio2.js
+++ desafio2.js
@@ -1,22 +1,25 @@
-let victory = 10
-let defeat = 5
+let victory = 100
+let defeat = 60
 let result = calculeOSaldoDeVitorias()
+let ranking;
 
 function calculeOSaldoDeVitorias(){
     subtracao = victory - defeat
     return subtracao
-    ranking()
 }
 
+definicao()
 function definicao(){
     if (result <= 10){
         ranking = "Ferro"
-    }else if (result <= 20){
+    }else if(result <= 20){
         ranking = "Bronze"
-    }else if (result <= 30){
+    }else if(result <= 30){
         ranking = "Prata"
-    }else if (result <= 40){
+    }else if(result <= 40){
         ranking = "Ouro"
+    }
+    return ranking
 }
 
 console.log("O ranking do Heroi é: "+ ranking)
