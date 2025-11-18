803d7c7eee23b98ac4891e21a7ca4f23d1816020
Lmb22 <lucas.belgoni@gmail.com>
2025-11-18 06:05:39 -0300

    outro desafio commit

--- desafio2.js
+++ desafio2.js
@@ -1,19 +1,22 @@
-let victory = 10
-let defeat = 5
-let result;
- 
+let victory = 0
+let defeat = 10
+let result = definition(victory - defeat)
 
-calculeOSaldoDeVitorias()
-
-function calculeOSaldoDeVitorias(){
-    subtracao = victory - defeat
-    return subtracao
-}
-
-function ranking(){
-    if (subtracao <= 10){
-        result = "Ferro"
-    }else if (subtracao <= 20){
-        result = "Bronze"
+function definition(subtraction){
+    if (subtraction >0 <= 10){
+        ranking = "Ferro"
+    }else if(subtraction <= 20){
+        ranking = "Bronze"
+    }else if(subtraction <= 50){
+        ranking = "Prata"
+    }else if(subtraction <= 80){
+        ranking = "Ouro"
+    }else if(subtraction <= 90){
+        ranking = "Diamante"
+    }else if(subtraction <= 100){
+        ranking = "Lendário"
+    }else{
+        ranking = "Imortal"
     }
+    console.log("O ranking do Heroi é: "+ ranking)
 }
