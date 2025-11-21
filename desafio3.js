f64b226d390090d396dbb797fb10fa3be4b2aace
Lucas Belgoni <lucas.belgoni@gmail.com>
2025-11-21 11:49:20 -0300

    Atualizar o desafio3.js

--- desafio3.js
+++ desafio3.js
@@ -1,12 +1,26 @@
-class recebendoHeroi{
-    constructor(nome, idade, tipo){
-        this.nome = nome;
-        this.idade = idade;
-        this.tipo = tipo;
+class getHero {
+    constructor(name, age, type){
+        this.name = name;
+        this.age = age;
+        this.type = type;
     }
-ataque() {
-    
-    }
-
+    brainHero(atackHero) {
+        switch (this.type){
+          case "Mago":
+          return "Magia"
+          break;
+          case "Guerreiro":
+          return "Espada"
+          break;
+          case "Monge":
+          return "artes marciais"
+          break;
+          case "Ninja":
+          return "Shuriken"
+          break;
+        }
     }
 }
+let hero1 = new getHero("Odin", "30", "Monge")
+let atackHero = hero1.brainHero()
+console.log(`O ${hero1.type} atacou usando ${atackHero}`)
